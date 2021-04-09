45748a4edc347144918d5317d67f9ccd11d20b07
Anis Islam <manisul1997bd@gmail.com>
2021-04-10 01:34:34 +0600

    payment intigration startted

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { UserContext } from '../../App';
 import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
+import ProcessPayment from '../ProcessPayment/ProcessPayment';
 import './Shipment.css'
 
 const Shipment = () => {
@@ -34,22 +35,31 @@ const Shipment = () => {
   console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-    <form className='ship-form' onSubmit={handleSubmit(onSubmit)}>
+    <div className="row">
+      <div className="col-md-6">
+        <form className='ship-form' onSubmit={handleSubmit(onSubmit)}>
 
-      <input name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your Name" />
-      {errors.name && <span className='error'>Name is required</span>}
+          <input name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your Name" />
+          {errors.name && <span className='error'>Name is required</span>}
 
-      <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your Email" />
-      {errors.name && <span className='error'>Email is required</span>}
+          <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your Email" />
+          {errors.name && <span className='error'>Email is required</span>}
 
-      <input name="address" ref={register({ required: true })} placeholder="Your Address" />
-      {errors.name && <span className='error'>Address is required</span>}
+          <input name="address" ref={register({ required: true })} placeholder="Your Address" />
+          {errors.name && <span className='error'>Address is required</span>}
 
-      <input name="phone" ref={register({ required: true })} placeholder="Your Phone" />
-      {errors.name && <span className='error'>Phone is required</span>}
+          <input name="phone" ref={register({ required: true })} placeholder="Your Phone" />
+          {errors.name && <span className='error'>Phone is required</span>}
 
-      <input type="submit" />
-    </form>
+          <input type="submit" />
+        </form>
+      </div>
+      <div className="col-md-6">
+        <h2>Please pay for me</h2>
+        <ProcessPayment></ProcessPayment>
+      </div>
+    </div>
+
   );
 };
 
